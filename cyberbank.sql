15a52b76e6e171f759d76a718efeb8724602a85c
ThatKoshevoy <kosh.evoy@yandex.ru>
2021-05-20 01:27:54 +0500

    Update cyberbank.sql

--- cyberbank.sql
+++ cyberbank.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 18, 2021 at 11:58 PM
+-- Generation Time: May 19, 2021 at 08:27 PM
 -- Server version: 5.7.24
 -- PHP Version: 7.4.1
 
@@ -42,10 +42,10 @@ CREATE TABLE `credit_cards` (
 --
 
 INSERT INTO `credit_cards` (`cr_c_id`, `cr_ca_cache`, `cr_c_need_cache`, `cr_c_cardnuber`, `cr_c_id_cvv`, `cr_c_id_user`) VALUES
-(1, 300, 300, '4081986265729662', '953', 4),
-(2, 450000, 450000, '1349479628728347', '382', 1),
 (3, 4500, 4500, '4794607430404170', '102', 3),
-(4, 120000, 120000, '3015997924655714', '407', 7);
+(4, 120000, 120000, '3015997924655714', '407', 7),
+(5, 9999, 10000, '9461113764377491', '537', 5),
+(6, 1254, 1254, '4194198144126600', '560', 6);
 
 -- --------------------------------------------------------
 
@@ -112,12 +112,12 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`u_id`, `u_username`, `u_password`, `u_name`, `u_surname`, `u_date_of_birth`, `u_havecart`, `u_havecredit`, `u_role`) VALUES
-(1, 'DTC5epRqdlXIYp8vG5FwAA==', 'DTC5epRqdlXIYp8vG5FwAA==', 'Wb7VeNcSY3vCpbUr+4ewXw==', 'HmEwX+0To7MVHNXLPiOGxg==', '2001-04-08', 1, 1, 'admin'),
+(1, 'DTC5epRqdlXIYp8vG5FwAA==', 'DTC5epRqdlXIYp8vG5FwAA==', 'Wb7VeNcSY3vCpbUr+4ewXw==', 'HmEwX+0To7MVHNXLPiOGxg==', '2001-04-08', 1, 0, 'admin'),
 (2, 'L51r9CbxieFS4Qew5g2RyQ==', 'sOYx3s8xME4m71PfVDlWJA==', 'Mmoca8mtxQJ/5SQ7gSuhUw==', 'ZhwLYvtdhSfxuZODt+OtfQ==', '1889-04-20', 1, 0, NULL),
 (3, 'Z+KNzFZtt9kPZ23hbrkQ1Q==', 'gBqHe27eDt5qqq0P00KpRQ==', 'NHu9fOFgQxu2Usj3cJQVZA==', '4eCEWu2ztG1PWJwhdvPqjw==', '2021-05-06', 1, 1, NULL),
-(4, 'yp5qKl1zONC5C7d9jsHpAQ==', 'RYc0hRCX5WB1bgfyB8kiOw==', 'QtluvBJVcsg4gIELNQBCtA==', 'I6/28JK6y3FTzqSea7nHCA==', '2001-06-18', 1, 1, NULL),
-(5, 'KXEEoKHbteJuDOieXU1qtg==', 'LonhFNY3MjNxfd6Xfv5vBA==', '5AenZeuhga0h67lYD5Tp9w==', 'SJgdYfSN6mecf569UJSvlQ==', '2001-10-11', 1, 0, NULL),
-(6, 'ufWBS4+P9z4JDHyKv3ZMWQ==', 'k7Nn1LIt5fkLO0TTqjwK1Q==', 'ufWBS4+P9z4JDHyKv3ZMWQ==', 'xt46mkP/Bu6oryJrj2PYIA==', '2021-08-04', 1, 0, NULL),
+(4, 'yp5qKl1zONC5C7d9jsHpAQ==', 'RYc0hRCX5WB1bgfyB8kiOw==', 'QtluvBJVcsg4gIELNQBCtA==', 'I6/28JK6y3FTzqSea7nHCA==', '2001-06-18', 1, 0, NULL),
+(5, 'KXEEoKHbteJuDOieXU1qtg==', 'LonhFNY3MjNxfd6Xfv5vBA==', '5AenZeuhga0h67lYD5Tp9w==', 'SJgdYfSN6mecf569UJSvlQ==', '2001-10-11', 1, 1, NULL),
+(6, 'ufWBS4+P9z4JDHyKv3ZMWQ==', 'k7Nn1LIt5fkLO0TTqjwK1Q==', 'ufWBS4+P9z4JDHyKv3ZMWQ==', 'xt46mkP/Bu6oryJrj2PYIA==', '2021-08-04', 1, 1, NULL),
 (7, 'LF91C4q39ogmiU+kPgB/VQ==', 'V0x5MQQ5Zuxv46EAu1J1zQ==', 'ah2uUWsOCh8lp1r0IRSoMA==', 'NMgT1l0pC2F8F2BQPgY39g==', '2000-12-29', 1, 1, NULL);
 
 --
@@ -159,13 +159,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `credit_cards`
 --
 ALTER TABLE `credit_cards`
-  MODIFY `cr_c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `cr_c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `credit_requests`
 --
 ALTER TABLE `credit_requests`
-  MODIFY `c_r_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `c_r_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `e_carts`
