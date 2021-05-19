94eec6ca1976a29b3390e9962279bb0660174582
ThatKoshevoy <kosh.evoy@yandex.ru>
2021-05-20 01:26:38 +0500

    meme

--- CyberBank/obj/Release/CyberBank_Content.g.i.cs
+++ CyberBank/obj/Release/CyberBank_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("carglogo.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("orange3.ico")]
 
 
